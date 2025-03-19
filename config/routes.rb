095debc767012148a088602cc3c1f8af189ad6b9
Ao65 <ryu17615@gmail.com>
2025-03-19 21:00:27 +0900

    chore: letteropenerのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   draw(:admin)
 
   get 'up' => 'rails/health#show', as: :rails_health_check
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
