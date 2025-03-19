5b018fc38def5d187ecb1a26a8a143e3be1a460c
Ao65 <ryu17615@gmail.com>
2025-03-20 04:30:17 +0900

    feat: productにバリデーションを追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,4 +10,7 @@
 #  updated_at       :datetime         not null
 #
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price_before_tax, presence: true, numericality: { greater_than_or_equal_to: 0, message: 'は0以上の値にしてください' }
 end
