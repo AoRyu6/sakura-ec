9e938c7cbcdc257800086e7b00593b7e2c08212b
Ao65 <ryu17615@gmail.com>
2025-03-20 05:15:34 +0900

    chore: adminでもN+1を検出する

--- app/controllers/admins/application_controller.rb
+++ app/controllers/admins/application_controller.rb
@@ -3,4 +3,15 @@ class Admins::ApplicationController < ActionController::Base
 
   before_action :authenticate_admin!
   layout 'admin/application'
+
+  unless Rails.env.production?
+    around_action :n_plus_one_detection
+
+    def n_plus_one_detection
+      Prosopite.scan
+      yield
+    ensure
+      Prosopite.finish
+    end
+  end
 end
