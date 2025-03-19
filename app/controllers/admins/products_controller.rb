48beca3f83d2cf24485c881b3fc7ca2b0f80d122
Ao65 <ryu17615@gmail.com>
2025-03-20 04:48:02 +0900

    feat: product#createの実装

--- app/controllers/admins/products_controller.rb
+++ app/controllers/admins/products_controller.rb
@@ -5,4 +5,20 @@ class Admins::ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admins_products_path, notice: '商品を登録しました'
+    else
+      render :new, status: :unprocessable_entity
+
+    end
+  end
+
+  private
+
+  def product_params
+    params.expect(product: %i[name price_before_tax description])
+  end
 end
