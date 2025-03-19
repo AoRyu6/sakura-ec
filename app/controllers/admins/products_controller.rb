a08a1415a3438df6f82e50b9cc6fc9aefb6df361
Ao65 <ryu17615@gmail.com>
2025-03-20 05:05:50 +0900

    wip: product#showの仮作成

--- app/controllers/admins/products_controller.rb
+++ app/controllers/admins/products_controller.rb
@@ -1,5 +1,9 @@
 class Admins::ProductsController < ApplicationController
+  before_action :set_product, only: %i[show]
   def index
+  end
+
+  def show
   end
 
   def new
@@ -17,6 +21,10 @@ class Admins::ProductsController < ApplicationController
   end
 
   private
+
+  def set_product
+    @product = Product.find(params.expect(:id))
+  end
 
   def product_params
     params.expect(product: %i[name price_before_tax description])
