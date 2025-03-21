078f9e4f437a273fa4d23326e9c3ee10c18e3359
Ao65 <ryu17615@gmail.com>
2025-03-21 13:50:29 +0900

    feat: 日記のページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users/registrations' }
-
   root 'home#index'
 
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   draw(:admin)
+
+  resources :diaries
 
   get 'up' => 'rails/health#show', as: :rails_health_check
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
