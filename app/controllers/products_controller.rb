bf9b6fcbc1c0f018384981ce34800735e8a83e02
Ao65 <ryu17615@gmail.com>
2025-03-21 15:04:16 +0900

    feat: products#indexページの作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
   def index
+    # TODO: - publicizeを実装する
+    @pagy, @products = pagy(Product.published.preload(image_attachment: :blob))
   end
 
   def show
