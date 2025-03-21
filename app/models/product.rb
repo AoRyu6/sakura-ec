53d7383d50e8968084cbe2d82f265359dbd87c7e
Ao65 <ryu17615@gmail.com>
2025-03-21 09:42:01 +0900

    feat: 画像にバリデーションを追加する

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,6 +12,9 @@
 class Product < ApplicationRecord
   has_one_attached :image
 
+  ACCEPTED_CONTENT_TYPES = %w[image/png image/jpeg image/gif image/tiff].freeze
+
   validates :name, presence: true
   validates :price_before_tax, allow_nil: true, numericality: { greater_than_or_equal_to: 0, message: 'は0以上の値にしてください' }
+  validates :image, content_type: ACCEPTED_CONTENT_TYPES, size: { less_than: 25.megabytes }
 end
