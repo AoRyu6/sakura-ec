dfd0274de7c8097c58fe08ae2629bdcd79c6822e
Ao65 <ryu17615@gmail.com>
2025-03-24 10:22:00 +0900

    chore: rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_23_103438) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_24_012051) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -70,6 +70,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_23_103438) do
     t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name", null: false
     t.text "description", default: "", null: false
@@ -104,4 +111,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_23_103438) do
   add_foreign_key "cart_items", "carts"
   add_foreign_key "cart_items", "products"
   add_foreign_key "carts", "users"
+  add_foreign_key "orders", "users"
 end
