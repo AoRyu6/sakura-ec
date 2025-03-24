d3f2d1b7d9cf8fd6bcf167d2080698bdbf1d223a
Ao65 <ryu17615@gmail.com>
2025-03-24 10:37:23 +0900

    chore: ユーザーとオーダーを関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   has_one :cart, dependent: :destroy
+  has_many :orders, dependent: :restrict_with_exception
 
   validates :name, presence: true
 end
