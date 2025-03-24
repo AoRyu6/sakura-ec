0ef6bea0344ffe1d58f74e9cb9d0e0f931cb18ee
Ao65 <ryu17615@gmail.com>
2025-03-24 10:31:57 +0900

    chore: rails db:migrate

--- db/migrate/20250324012234_create_order_items.rb
+++ db/migrate/20250324012234_create_order_items.rb
@@ -3,7 +3,7 @@ class CreateOrderItems < ActiveRecord::Migration[8.0]
     create_table :order_items do |t|
       t.references :order, null: false, foreign_key: true
       t.references :product, null: false, foreign_key: true
-      t.integer :price
+      t.monetize :price, amount: { null: false, default: nil }
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_24_012051) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_24_012234) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -70,6 +70,17 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_24_012051) do
     t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
+  create_table "order_items", force: :cascade do |t|
+    t.bigint "order_id", null: false
+    t.bigint "product_id", null: false
+    t.integer "price_cents", null: false
+    t.string "price_currency", default: "JPY", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["product_id"], name: "index_order_items_on_product_id"
+  end
+
   create_table "orders", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
@@ -111,5 +122,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_24_012051) do
   add_foreign_key "cart_items", "carts"
   add_foreign_key "cart_items", "products"
   add_foreign_key "carts", "users"
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "order_items", "products"
   add_foreign_key "orders", "users"
 end
