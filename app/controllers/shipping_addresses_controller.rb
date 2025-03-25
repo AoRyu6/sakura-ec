9d159ae78d9f7d15aa95a7d9be100328f7e774b9
Ao65 <ryu17615@gmail.com>
2025-03-26 00:06:40 +0900

    feat: 配送先編集機能

--- app/controllers/shipping_addresses_controller.rb
+++ app/controllers/shipping_addresses_controller.rb
@@ -1,10 +1,15 @@
 class ShippingAddressesController < ApplicationController
+  before_action :set_shipping_address, only: %i[edit update]
+
   def index
     @shipping_addresses = current_user.shipping_addresses
   end
 
   def new
     @shipping_address = current_user.shipping_addresses.new
+  end
+
+  def edit
   end
 
   def create
@@ -17,7 +22,19 @@ class ShippingAddressesController < ApplicationController
     end
   end
 
+  def update
+    if @shipping_address.update(shipping_address_params)
+      redirect_to shipping_addresses_path, notice: '配送先住所を更新しました'
+    else
+      render :edit, status: :unprocessable_entity, notice: '更新に失敗しました'
+    end
+  end
+
   private
+
+  def set_shipping_address
+    @shipping_address = current_user.shipping_addresses.find(params.expect(:id))
+  end
 
   def shipping_address_params
     params.expect(shipping_address: %i[recipient_name postal_code prefecture_id city street_address])
