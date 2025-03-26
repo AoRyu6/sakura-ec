22bf57fff4183901add590a0fa93bb276d7a9909
Ao65 <ryu17615@gmail.com>
2025-03-26 11:03:05 +0900

    feat: ユーザーの履歴機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,12 @@
 class OrdersController < ApplicationController
+  def index
+    @pagy, @orders = pagy(current_user.orders.preload(:order_items, :products).default_order)
+  end
+
+  def show
+    @order = current_user.orders.find(params[:id])
+  end
+
   def create
     current_cart.order!
     redirect_to root_path, notice: '注文を受け付けました。メール内容をご確認ください。'
